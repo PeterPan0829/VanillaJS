12d4a3fbb00949341595a0578d15d31d25132b89
許家翔 <sjshiang@hotmail.com>
2018-07-22 18:15:56 +0800

    " 新增 hash "

---
+++ Final_WebSite/05-page/ui.js
@@ -0,0 +1,9 @@
+const ui = {}
+
+ui.html = function (path, html) {
+  document.querySelector(path).innerHTML = html //querySelector() 只會找第一個，這邊是找出 path 第一個後把 innerHTML 設成 HTML
+}
+
+ui.show = function (html) { // ui.show 執行是把 html 網頁的內容填到 "main" 主區塊
+  ui.html('#main', html)
+}
