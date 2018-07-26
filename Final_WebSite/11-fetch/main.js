7ca8374686ea86f6f4e848fc289d0ada798a7af4
許家翔 <sjshiang@hotmail.com>
2018-07-26 15:31:26 +0800

    " 新增 fetch （把 10-map 改用 fetch 到後端抓網頁來填入） "

--- Final_WebSite/11-fetch/main.js
+++ Final_WebSite/11-fetch/main.js
@@ -6,15 +6,19 @@ window.onhashchange = function () {
   let hash = window.location.hash.substring(1).toLowerCase()
   console.log('hash=', hash)
   if (hash.length === 0) hash = 'home'
-  fetch('page/' + hash + '.html').then(function(response) {
+  fetch('page/' + hash + '.html').then(function (response) {
     console.log('response=', response)
     response.text().then(function (text) {
       console.log('text=', text)
       ui.show(text)
       switch (hash) {
-        case 'chart_simple': chart.simple(); break;
-        case 'map_simple': map.simple(); break;
-      }      
+        case 'chart_simple':
+          chart.simple();
+          break;
+        case 'map_simple':
+          map.simple();
+          break;
+      }
     })
   })
 }
--- Final_WebSite/11-fetch/map.js
+++ Final_WebSite/11-fetch/map.js
@@ -1,7 +1,10 @@
 const map = {}
 
 map.simple = function () {
-  var pos = {lat: 24.45, lng: 118.32};
+  var pos = {
+    lat: 24.45,
+    lng: 118.32
+  };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: pos
